ea7896ffa5f80ff51417e1ddf9d9795236895bd1
Антон <antonolyshevets@gmail.com>
2024-12-17 21:58:32 +0200

    connect

--- Backend_Movies_API/index.js
+++ Backend_Movies_API/index.js
@@ -2,37 +2,50 @@
 // │
 // ├── node_modules/         # Сюди нічого не пишемо
 // ├── .env                  # Тут зберігаються креденшали
+// ├── .gitignore
 // ├── package.json          # Інформація про залежності проекту
 // ├── index.js              # Головний файл проекту
 // └── script.js             # Ваші інші файли коду
 // // examle ()
 
-// import { MONGO_USERNAME, MONGO_PASSWORD } from "./cred.js";
-
-
-// console.log(`MongoDB Username: ${MONGO_USERNAME}`);
-// console.log(`MongoDB Password: ${MONGO_PASSWORD}`);
-
 require('dotenv').config();
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME;
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-
-console.log('Username:', MONGO_USERNAME);
-console.log('Password:', MONGO_PASSWORD);
+// console.log('Username:', MONGO_USERNAME);
+// console.log('Password:', MONGO_PASSWORD);
 
 
-// import app from  "./server.js"
-// import mongodb from "mongodb"
-// import ReviewsDAO from "./dao/reviewsDAO.js"
+import app from  "./server.js"
+import mongodb from "mongodb"
+import ReviewsDAO from "./dao/reviewsDAO.js"
 
 
-// const MongoClient = mongodb.MongoClient
-// const mongo_username = MONGO_USERNAME
-// const mongo_password = MONGO_PASSWORD
+const MongoClient = mongodb.MongoClient
+const MONGO_USERNAME = process.env["MONGO_USERNAME"];
+const MONGO_PASSWORD = process.env["MONGO_PASSWORD"];
+const uri = 
+`mongodb+srv://${MONGO_USERNAME}:
+${MONGO_PASSWORD}@cluster0.mlcfl.
+mongodb.net/`
 
 
+const port = 8000
 
+MongoClient.connect(
+    uri,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true,
+    })
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(async client =>{
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
 
 
 
